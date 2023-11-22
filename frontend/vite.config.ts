351dc5362112ba1518d2aa1b0dc75b1d1c154771
Melinda Cortez <meli.matrix@outlook.com>
2023-11-22 12:29:18 -0800

    Add pwa capability

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgLoader(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: "./public",
+      manifest: {
+        theme_color: "#CC7E85",
+        icons: [
+          {
+            sizes: "512x512",
+            src: "favicon.svg",
+          },
+          {
+            sizes: "512x512",
+            src: "./src/assets/icon-512.png",
+          },
+          {
+            src: "./src/assets/maskable_icon_x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
